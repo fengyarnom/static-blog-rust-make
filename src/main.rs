48fc5674c006e5da3191f83f61adbee488bf52f4
yarnom <fengyarnom@gmail.com>
2023-08-02 23:17:26 +0800

    feat: Add new feature
    This is the first version of the project:[version 0.1.1]. It will be capable of parsing Markdown format files and rendering HTML files based on templates.
    
    + parse markdown file
    + render html file

--- src/main.rs
+++ src/main.rs
@@ -19,117 +19,115 @@ struct Post {
     content:String
 }
 
-fn main() {
+fn generate_html_from_mardown(){
     let posts_path = Path::new("./posts");
     if let Ok(entries) = Path::read_dir(posts_path){
-        let matter = Matter::<YAML>::new();
         for entry in entries {
             if let Ok(entry) = entry{
                 if let Some(extension) = entry.path().extension(){
                     if extension == "md"{
                         let raw_content = fs::read_to_string(entry.path()).unwrap();
-                        let result = matter.parse(&raw_content);
+                        let post = parse_markdown(&raw_content).unwrap();
 
-                        let frontmatter = result.data.as_ref().unwrap().as_hashmap().unwrap();
-                        let markdown_content = result.content;
-                        let html_content = markdown_to_html(&markdown_content, &ComrakOptions::default());
-
-                        let mut post = Post{
-                            title: "".to_string(),
-                            date: "".to_string(),
-                            tags: vec![],
-                            categories: vec![],
-                            content: html_content,
-                        };
-
-                        for item_frontmatter in &frontmatter {
-                            match item_frontmatter.0.as_str() {
-                                "title" => {
-                                    post.title = item_frontmatter.1.as_string().unwrap();
-                                },
-                                "date" => {
-                                    post.date = item_frontmatter.1.as_string().unwrap();
-                                },
-                                "tags" => {
-                                    if !item_frontmatter.1.is_empty(){
-                                        for tag in item_frontmatter.1.as_vec().unwrap(){
-                                            post.tags.push(tag.as_string().unwrap())
-                                        }
-                                    }
-                                },
-
-                                "categories" => {
-                                    if !item_frontmatter.1.is_empty(){
-                                        for category in item_frontmatter.1.as_vec().unwrap(){
-                                            post.categories.push(category.as_string().unwrap())
-                                        }
-                                    }
-                                },
-                                _ => println!("Unknown frontmatter item"),
-                            }
-
-                        }
-
-                        println!("{:?}",post);
-
-                        let tera = match Tera::new("templates/**/*") {
-                            Ok(t) => t,
-                            Err(e) => {
-                                println!("Error initializing Tera: {:?}", e);
-                                return;
-                            }
-                        };
-
-                        // 创建数据上下文
-                        let mut context = Context::new();
-                        context.insert("title", &post.title);
-                        context.insert("content", &tera::Value::String(post.content.to_string()));
-                        context.insert("date", &post.date);
-
-                        let result = tera.render("post.html", &context);
-
-                        match result {
-                            Ok(rendered) => {
-                                println!("Rendered content:\n{}", rendered);
-                                let file_path = "output.html";
-
-                                let mut file = match File::create(file_path) {
-                                    Ok(file) => file,
-                                    Err(e) => {
-                                        println!("Error creating file: {:?}", e);
-                                        return;
-                                    }
-                                };
-
-                                match file.write_all(rendered.as_bytes()) {
-                                    Ok(_) => println!("HTML file generated successfully!"),
-                                    Err(e) => println!("Error writing to file: {:?}", e),
-                                }
-                            },
-                            Err(e) => println!("Error rendering template: {:?}", e),
-                        }
-
-
+                        render_templates(&post);
                     }
                 }
             }
-
         }
-    }else{
-        println!("Failed to read the directory.");
+    }
+}
+fn parse_markdown(raw_content: &str) -> Option<Post>{
+    let matter = Matter::<YAML>::new();
+    let parsed_entity = matter.parse(&raw_content);
+
+    let frontmatter = parsed_entity.data.as_ref().unwrap().as_hashmap().unwrap();
+    let markdown_content = parsed_entity.content;
+    let html_content = parse_markdown2html(&markdown_content).unwrap();
+
+    let mut post = Post{
+        title: "".to_string(),
+        date: "".to_string(),
+        tags: vec![],
+        categories: vec![],
+        content: html_content,
     };
 
+    for item_frontmatter in &frontmatter {
+        match item_frontmatter.0.as_str() {
+            "title" => {
+                post.title = item_frontmatter.1.as_string().unwrap();
+            },
+            "date" => {
+                post.date = item_frontmatter.1.as_string().unwrap();
+            },
+            "tags" => {
+                if !item_frontmatter.1.is_empty(){
+                    for tag in item_frontmatter.1.as_vec().unwrap(){
+                        post.tags.push(tag.as_string().unwrap())
+                    }
+                }
+            },
 
+            "categories" => {
+                if !item_frontmatter.1.is_empty(){
+                    for category in item_frontmatter.1.as_vec().unwrap(){
+                        post.categories.push(category.as_string().unwrap())
+                    }
+                }
+            },
+            _ => println!("Unknown frontmatter item"),
+        }
 
+    }
 
+    Some(post)
 
+}
 
-    // let matter = Matter::<YAML>::new();
-    // let result = matter.parse(INPUT);
-    //
-    //
-    // // Deserialize `result` manually:
-    // let front_matter: FrontMatter = result.data.unwrap().deserialize().unwrap();
-    // println!("{:?}", front_matter);
+fn render_templates(post: &Post){
+    let tera = match Tera::new("./templates/**/*") {
+        Ok(t) => t,
+        Err(e) => {
+            println!("Error initializing Tera: {:?}", e);
+            return;
+        }
+    };
 
+    // 创建数据上下文
+    let mut context = Context::new();
+    context.insert("title", &post.title);
+    context.insert("content", &tera::Value::String(post.content.to_string()));
+    context.insert("date", &post.date);
+
+    let result = tera.render("post.html", &context);
+
+    match result {
+        Ok(rendered) => {
+            println!("Rendered content:\n{}", rendered);
+            let file_path = "output.html";
+
+            let mut file = match File::create(file_path) {
+                Ok(file) => file,
+                Err(e) => {
+                    println!("Error creating file: {:?}", e);
+                    return;
+                }
+            };
+
+            match file.write_all(rendered.as_bytes()) {
+                Ok(_) => println!("HTML file generated successfully!"),
+                Err(e) => println!("Error writing to file: {:?}", e),
+            }
+        },
+        Err(e) => println!("Error rendering template: {:?}", e),
+    }
+}
+
+fn parse_markdown2html(markdown_content: &str) -> Option<String>{
+    let html_content = markdown_to_html(&markdown_content, &ComrakOptions::default());
+    Some(html_content)
+}
+
+fn main() {
+    generate_html_from_mardown();
 }
