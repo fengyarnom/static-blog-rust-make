6922fef6509a72a2d3ce9f0b359d4bddd9f55607
yarnom <fengyarnom@gmail.com>
2023-08-02 14:06:57 +0800

    test: Sucessed to parse markdown front matter to the post data sturcture

---
+++ posts/a.md
@@ -0,0 +1,10 @@
+---
+title: Hello
+date: 2018-05-20 20:37:16
+tags:
+categories:
+  - defalut
+---
+
+# Hello World
+This is a content.
--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,95 @@ use gray_matter::Matter;
 use gray_matter::engine::YAML;
 use serde::Deserialize;
 
+use std::fs;
+use std::path::Path;
+
 #[derive(Deserialize, Debug)]
-struct FrontMatter {
+struct Post {
     title: String,
-    tags: Vec<String>
+    date: String,
+    tags: Vec<String>,
+    categories: Vec<String>,
+    content:String
 }
 
-const INPUT: &str = r#"---
-title: gray-matter-rs
-tags:
-  - gray-matter
-  - rust
----
-Some excerpt
----
-Other stuff
-"#;
-
 fn main() {
-    let matter = Matter::<YAML>::new();
-    let result = matter.parse(INPUT);
+    let posts_path = Path::new("./posts");
+    if let Ok(entries) = Path::read_dir(posts_path){
+        let matter = Matter::<YAML>::new();
+        for entry in entries {
+            if let Ok(entry) = entry{
+                if let Some(extension) = entry.path().extension(){
+                    if extension == "md"{
+                        let raw_content = fs::read_to_string(entry.path()).unwrap();
+                        let result = matter.parse(&raw_content);
+
+                        let frontmatter = result.data.as_ref().unwrap().as_hashmap().unwrap();
+                        let markdown_content = result.content;
+
+                        let mut post = Post{
+                            title: "".to_string(),
+                            date: "".to_string(),
+                            tags: vec![],
+                            categories: vec![],
+                            content: "".to_string(),
+                        };
+
+                        for item_frontmatter in &frontmatter {
+                            match item_frontmatter.0.as_str() {
+                                "title" => {
+                                    post.title = item_frontmatter.1.as_string().unwrap();
+                                },
+                                "date" => {
+                                    post.date = item_frontmatter.1.as_string().unwrap();
+                                },
+                                "tags" => {
+                                    if !item_frontmatter.1.is_empty(){
+                                        for tag in item_frontmatter.1.as_vec().unwrap(){
+                                            post.tags.push(tag.as_string().unwrap())
+                                        }
+                                    }
+                                },
+
+                                "categories" => {
+                                    if !item_frontmatter.1.is_empty(){
+                                        for category in item_frontmatter.1.as_vec().unwrap(){
+                                            post.categories.push(category.as_string().unwrap())
+                                        }
+                                    }
+                                },
+                                _ => println!("Unknown frontmatter item"),
+                            }
+
+                        }
+                        
+                        println!("{:?}",post);
+                        // if let Ok(front_matter) =  result.data.unwrap().deserialize::<FrontMatter>(){
+                        //     println!("{:?}", front_matter);
+                        // }else{
+                        //     println!("{} missed matter",entry.path().to_string_lossy());
+                        // };
+
+                    }
+                }
+            }
+
+        }
+    }else{
+        println!("Failed to read the directory.");
+    };
 
 
-    // Deserialize `result` manually:
-    let front_matter: FrontMatter = result.data.unwrap().deserialize().unwrap();
-    println!("{:?}", front_matter);
+
+
+
+
+    // let matter = Matter::<YAML>::new();
+    // let result = matter.parse(INPUT);
+    //
+    //
+    // // Deserialize `result` manually:
+    // let front_matter: FrontMatter = result.data.unwrap().deserialize().unwrap();
+    // println!("{:?}", front_matter);
 
 }
