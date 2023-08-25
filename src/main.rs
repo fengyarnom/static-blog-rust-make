ae30856047b77d94d59ad7df590d643d539ac052
Yarnom <fengyarnom@gmail.com>
2023-08-25 21:32:53 +0800

    feat: Monitor File Changes

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,16 @@ use std::io::Write;
 use actix_web::{get, web, App, HttpServer, Responder};
 use actix_files::Files;
 use crate::generate::SiteGenerator;
-use pulldown_cmark::{Parser, Options, html};
-// #[actix_web::main] // or #[tokio::main]
-// async fn main() -> std::io::Result<()> {
-//     let file_handling = FileHandler::new().unwrap();
-//     let mut posts = post_parse(&file_handling).unwrap();
-//
-//     Page::render_static_source();
-//     //render_html(&file_handling,&mut posts);
-//
-//     HttpServer::new(|| {
-//         App::new().service(Files::new("/", "public").index_file("index.html"))
-//     })
-//     .bind(("127.0.0.1", 8080))?
-//     .run()
-//     .await
-// }
+
+use notify::{RecommendedWatcher, Watcher, RecursiveMode, Result};
+use std::sync::mpsc::channel;
+use std::time::Duration;
+use std::error::Error;
+use std::path::Path;
+use tokio::spawn;
+
+
+
 fn generate() {
     // 实现生成操作的逻辑
     println!("Generating...");
@@ -39,12 +33,26 @@ async fn server() -> std::io::Result<()> {
     // 实现服务器启动操作的逻辑
     println!("Starting server...");
     generate();
+    println!("Running server!");
+    let mut watcher = notify::recommended_watcher(|res| {
+        match res {
+            Ok(event) => {
+                generate();
+                println!("Restarted server!");
+            },
+            Err(e) => println!("watch error: {:?}", e),
+        }
+    }).unwrap();
+    watcher.watch(Path::new("."), RecursiveMode::Recursive).unwrap();
+
     HttpServer::new(|| {
         App::new().service(Files::new("/", "public").index_file("index.html"))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+
+
 }
 
 #[actix_web::main]
@@ -64,6 +72,8 @@ async fn main() {
                 eprintln!("Error starting server: {}", e);
                 process::exit(1);
             }
+            println!("hey");
+
         }
         _ => println!("Unknown command: {}", command),
     }
